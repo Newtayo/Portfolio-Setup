99c9cf59b2497fc5a4ddadf2825d4dc2b84c77a7
Newtayo <akandeabdul@gmail.com>
2023-01-31 14:02:23 +0100

    Implement toggle button changes for close and open menu

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const dropDown = document.querySelector('.dropdown')
 const mobileMenu = document.querySelector('.mobileMenu')
+
+var immage = document.querySelector('.immage');
 dropDown.addEventListener('click', () => {
+
   dropDown.classList.toggle('active')
   mobileMenu.classList.toggle('active')
+  immage.classList.toggle('active')
+  
 })
